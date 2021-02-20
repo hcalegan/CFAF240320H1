de71d47025516ab3bb6620bfe1a61c34856a26c5
hcalegan <58494285+hcalegan@users.noreply.github.com>
2021-02-20 14:52:11 -0500

    Update atmega328.h

--- atmega328/atmega328.h
+++ atmega328/atmega328.h
@@ -1,10 +1,11 @@
-#ifndef __ATMEGA2561_H__
-#define __ATMEGA2561_H__
+#ifndef __ATMEGA2561_H__ //__ESP32_H__
+#define __ATMEGA2561_H__ //__ESP32_H__
 
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <avr/pgmspace.h>
-//#include "../MMC_SD.h"
+#include <avr/io.h> //remove
+#include <avr/interrupt.h> //remove
+#include <avr/pgmspace.h> //remove
+//#include "../MMC_SD.h" //remove
+//#include <Arduino.h>
 
 //extern uint16_t SectorsPerClust;
 //extern uint16_t FirstDataSector;
@@ -14,15 +15,15 @@
 //struct direntry PictureInfo;
 
 
-#define CLR_CS     (PORTB &= ~(0x01)) //pin #8  - Chip Enable Signal
-#define SET_CS     (PORTB |=  (0x01)) //pin #8  - Chip Enable Signal
-#define CLR_RS     (PORTB &= ~(0x02)) //pin #9  - Data/Instruction
+#define CLR_CS     (PORTB &= ~(0x01)) //pin #8  - Chip Enable Signal //GPIO13
+#define SET_CS     (PORTB |=  (0x01)) //pin #8  - Chip Enable Signal 
+#define CLR_RS     (PORTB &= ~(0x02)) //pin #9  - Data/Instruction //?????? maybe GPIO12 (D/C)
 #define SET_RS     (PORTB |=  (0x02)) //pin #9  - Data/Instruction
-#define CLR_WR	   (PORTB &= ~(0x04)) //pin #10 - Read/Write
+#define CLR_WR	   (PORTB &= ~(0x04)) //pin #10 - Read/Write //GPIO14
 #define SET_WR	   (PORTB |=  (0x04)) //pin #10 - Read/Write
-#define CLR_RD	   (PORTB &= ~(0x08)) //pin #11 - 
+#define CLR_RD	   (PORTB &= ~(0x08)) //pin #11 - //GPIO27
 #define SET_RD	   (PORTB |=  (0x08)) //pin #11 - 
-#define CLR_RESET  (PORTB &= ~(0x10)) //pin #12 - Reset
+#define CLR_RESET  (PORTB &= ~(0x10)) //pin #12 - Reset //GPIO23
 #define SET_RESET  (PORTB |=  (0x10)) //pin #12 - Reset
 
 
@@ -47,4 +48,4 @@ void setRESET(void);
 void uPInit(char* interfaceType);
 uint8_t readData(void);
 
-#endif /* __ATMEGA2561_H__ */
+#endif /* __ATMEGA2561_H__ */ //__ESP32_H__
